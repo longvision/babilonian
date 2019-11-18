57f001dc0c228ce6dde30515b930e22a4a710e8c
Ricardo <r.n.hori@gmail.com>
2019-11-18 19:44:52 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './src';
+import { name as appName } from './app.json.js';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ ios/Pods/Target Support Files/Pods-Babilonian/Pods-Babilonian-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Babilonian : NSObject
+@end
+@implementation PodsDummy_Pods_Babilonian
+@end
